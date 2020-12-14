61258d0837c091030661b55ec2566504db82d6bc
Yogesh Reddy <yrkoppula@gmail.com>
2020-12-14 20:07:00 +0530

    avro-csv

---
+++ avro-csv.py
@@ -0,0 +1,20 @@
+#AVRO - CSV FILE CONVERSION USING PYTHON:
+----------------------------------------
+#install necessary modules
+pip install fastavro
+pip install python-snappy
+
+#import required modules
+from fastavro import reader
+import csv
+# coding part
+head = True
+fo = csv.writer(open("test.csv", "w+"))
+with open('abc.avro', 'rb') as fi:
+        avro_reader = reader(fi)
+        for data in avro_reader:
+        if head == True:
+            header = data.keys()
+            fo.writerow(header)
+            head = False
+        fo.writerow(data.values())
